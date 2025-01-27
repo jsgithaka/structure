7adfa5308b9f641a11075f47cc271bdf25fa1b14
jsgithaka <jsgithaka@gmail.com>
2025-01-27 13:29:59 +0300

    feat(components/layout): mount `Header` and `Main` components

--- src/components/layout/Page/Page.tsx
+++ src/components/layout/Page/Page.tsx
@@ -1,5 +1,11 @@
 import styles from './Page.module.css';
+import { Header, Main } from '@structureComponents';
 
 export default function Page() {
-    return <body className={styles.Page}></body>;
+    return (
+        <body className={styles.Page}>
+            <Header />
+            <Main />
+        </body>
+    );
 }
