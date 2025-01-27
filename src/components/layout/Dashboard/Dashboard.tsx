d6b2eb04df7fa962fb8b814371c4483cb422041e
jsgithaka <jsgithaka@gmail.com>
2025-01-27 13:32:11 +0300

    feat(components/layout): mount `Header`, `Main` and `Aside` components

--- src/components/layout/Dashboard/Dashboard.tsx
+++ src/components/layout/Dashboard/Dashboard.tsx
@@ -1,5 +1,12 @@
 import styles from './Dashboard.module.css';
+import { Aside, Header, Main } from '@structureComponents';
 
 export default function Dashboard() {
-    return <body className={styles.Dashboard}></body>;
+    return (
+        <body className={styles.Dashboard}>
+            <Header />
+            <Main />
+            <Aside />
+        </body>
+    );
 }
