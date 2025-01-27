4b15d42c970d64a9ad30a0ed3d6751dc53f77446
jsgithaka <jsgithaka@gmail.com>
2025-01-27 15:50:46 +0300

    feat(components/shared): create and export `Trigger` component

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,2 +1,3 @@
 export * from './layout/layout';
 export * from './structure/structure';
+export * from './shared/shared';
---
+++ src/components/shared/Trigger/Trigger.tsx
@@ -0,0 +1,5 @@
+import styles from './Trigger.module.css';
+
+export default function Trigger() {
+    return <div className={styles.Trigger}></div>;
+}
