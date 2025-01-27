c2d4de98f2654334fce0a96a3e54e410071d3d82
jsgithaka <jsgithaka@gmail.com>
2025-01-27 16:34:03 +0300

    feat(components/shared): update `Trigger` component

--- src/components/shared/Trigger/Trigger.tsx
+++ src/components/shared/Trigger/Trigger.tsx
@@ -1,5 +1,31 @@
+'use client';
+import { useState, useEffect } from 'react';
+
 import styles from './Trigger.module.css';
 
 export default function Trigger() {
-    return <div className={styles.Trigger}></div>;
+    const [aside, setAside] = useState<boolean>(false);
+    const triggerAside = () => {
+        setAside(!aside);
+    };
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const targetNode = document.querySelector('aside');
+            if (aside) {
+                targetNode?.setAttribute('style', 'display: flex');
+            } else {
+                targetNode?.setAttribute('style', 'display: none');
+            }
+        }
+    }, [aside]);
+
+    return (
+        <div
+            className={styles.Trigger}
+            onClick={triggerAside}
+        >
+            <p style={{ color: 'white' }}>Trigger Aside</p>
+        </div>
+    );
 }
